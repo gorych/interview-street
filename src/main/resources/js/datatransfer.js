fbf628ce408c1318f84e8ee4264dcd78223399cc
Egor Semenchenia <gorych911@gmail.com>
2015-11-17 00:58:34 +0300

    Added dynamic creation questions

--- src/main/resources/js/datatransfer.js
+++ src/main/resources/js/datatransfer.js
@@ -2,38 +2,73 @@ $(document).ready(function () {
     $('#addQuestionBtn').click(function () {
         $.ajax({
             url: '/create-question',
-            method: 'GET',
+            method: 'GET'
         }).done(function (questionId) {
             if (questionId > -1) {
-                $("#interview_questions").append("<section class='question'><h5 class='header black-text'>How do you do?</h5></section>");
+                var answersBoxId = "answers" + questionId;
+                var questionCssId = "#" + questionId;
+
+                $('<form/>', {
+                    id: questionId,
+                    class: 'question'
+                }).appendTo('#interview_questions');
+
+                $(questionCssId).append("<div class='row'><div class='input-field col s12'>" +
+                    "<input id='questionText' type='text' length='200'>" +
+                    "<label for='questionText'>Question</label></div></div>");
+
+                $('<div/>', {
+                    id: answersBoxId
+                }).appendTo(questionCssId);
+
+                $('<a/>', {
+                    id: "addAnswerBtn",
+                    class: 'btn-floating btn-middle waves-effect waves-light green'
+                }).appendTo(questionCssId).click(function () {
+                    $.ajax({
+                        url: '/create-answer',
+                        method: 'GET'
+                    }).done(function (answerId) {
+                        if (answerId > -1) {
+                            addAnswer(answerId, answersBoxId);
+                        }
+                    });
+                });
+
+                $("#addAnswerBtn").append("<i class='material-icons'>add</i>");
+
+                $('<div/>', {
+                    class: 'divider divider-margin-fix'
+                }).appendTo(questionCssId);
+
+                $('<div/>', {
+                    class: 'right-align'
+                }).appendTo(questionCssId).append("" +
+                    "<a href='#' class='modal-action modal-close waves-effect waves-green btn-flat'>Save</a>" +
+                    "<a href='#' class='modal-action modal-close waves-effect waves-red btn-flat'>Delete</a>");
             }
         });
     });
 });
 
+function addAnswer(answerId, parentId) {
+    var parentCssId = "#" + parentId;
+    var typeId = "type" + answerId;
+    var textId = "text" + answerId;
 
+    $(parentCssId).append("<div id='" + answerId + "' class='row'><div class='input-field col l5 m5'>" +
+        "<input id='" + textId + "' type='text'>" +
+        "<label for='" + textId + "'>Answer type</label>" +
+        "</div><div class='input-field col l6 m6'>" +
+        "<input id='" + typeId + "' type='text'>" +
+        "<label for='" + typeId + "'>Answer text</label>" +
+        "</div></div>");
 
-
-/*<section>
-<div class="question">
-    <h5 class="header black-text">Ваши предложения</h5>
-<div class="answers">
-    <form action="#">
-    <div class="row">
-    <div class="input-field col s6">
-    <input id="first_name" type="text" class="validate">
-    <label for="first_name">Имя</label>
-    </div>
-    <div class="input-field col s6">
-    <input id="last_name" type="text" class="validate">
-    <label for="last_name">Фамилия</label>
-    </div>
-    <div class="input-field col s12">
-    <input id="password" type="password" class="validate">
-    <label for="password">Ваши предложения</label>
-</div>
-</div>
-</form>
-</div>
-</div>
-</section>*/
+    var elementCssId = "#" + answerId;
+    $('<a/>', {
+        class: 'btn-floating btn-large waves-effect amber darken-3',
+        href:"#"
+    }).appendTo(elementCssId).append("<i class='material-icons'>delete</i>").click(function () {
+        $(elementCssId).remove();
+    });
+}

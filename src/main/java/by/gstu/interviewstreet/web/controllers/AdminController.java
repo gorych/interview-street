345dd5245c9c5a524001d360be1886b2f628aa86
Egor Semenchenia <gorych911@gmail.com>
2015-11-17 00:56:40 +0300

    Added createNewAnswer method

--- src/main/java/by/gstu/interviewstreet/web/controllers/AdminController.java
+++ src/main/java/by/gstu/interviewstreet/web/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package by.gstu.interviewstreet.web.controllers;
 
 
+import by.gstu.interviewstreet.service.AnswerService;
 import by.gstu.interviewstreet.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,9 @@ public class AdminController {
 
     @Autowired
     QuestionService questionService;
+
+    @Autowired
+    AnswerService answerService;
 
     @RequestMapping(value = {"/interview-list"}, method = RequestMethod.GET)
     public String goToInterviews() {
@@ -28,5 +32,11 @@ public class AdminController {
     @ResponseBody
     public long createNewQuestion() {
         return questionService.insertQuestion();
+    }
+
+    @RequestMapping(value = {"/create-answer"}, method = RequestMethod.GET)
+    @ResponseBody
+    public long createNewAnswer() {
+        return answerService.insertAnswer();
     }
 }

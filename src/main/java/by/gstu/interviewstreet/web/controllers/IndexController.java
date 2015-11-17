d421992565863ed636da6b8c3f3e53be2b1a8efc
Egor Semenchenia <gorych911@gmail.com>
2015-11-17 23:24:55 +0300

    Added gateway method

--- src/main/java/by/gstu/interviewstreet/web/controllers/IndexController.java
+++ src/main/java/by/gstu/interviewstreet/web/controllers/IndexController.java
@@ -1,11 +1,18 @@
 package by.gstu.interviewstreet.web.controllers;
 
 
+import by.gstu.interviewstreet.security.UserRoleConstants;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
 
 @Controller
 public class IndexController {
@@ -17,6 +24,18 @@ public class IndexController {
         }
         return "login";
 
+    }
+
+    @RequestMapping(value = {"/gateway"}, method = RequestMethod.GET)
+    public String gateway() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        Collection roles = authentication.getAuthorities();
+
+        boolean isEditor = roles.contains(new SimpleGrantedAuthority(UserRoleConstants.EDITOR));
+        return isEditor ?
+                "redirect:/interview-list" :
+                "redirect:/interviews";
     }
 
 }

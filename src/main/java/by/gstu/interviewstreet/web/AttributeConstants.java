8d5ec7cca4e274acca64125e87eb0b55bb3dd04d
Egor Semenchenia <gorych911@gmail.com>
2015-11-17 23:15:45 +0300

    Added new constant

--- src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
+++ src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
@@ -2,6 +2,8 @@ package by.gstu.interviewstreet.web;
 
 public final class AttributeConstants {
 
+    public static final String USER_INITIALS = "user_initials";
+
     private AttributeConstants() {
     }
 

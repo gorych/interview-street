3f6b016bfa3d967dcc2100433129fb63c3b0b40e
Egor Semenchenia <gorych911@gmail.com>
2015-11-20 00:03:57 +0300

    Added script for data to select loading

--- src/main/resources/js/dataload.js
+++ src/main/resources/js/dataload.js
@@ -1,4 +1,4 @@
-function getSelectValues(select) {
+function loadEmployeePosts(select) {
     var result = [];
     var options = select && select.options;
 
@@ -10,7 +10,24 @@ function getSelectValues(select) {
 
     $.ajax({
         url: '/load-posts',
-        method: 'POST',
-        data: {"ids": result}
+        method: 'GET',
+        data: {"data": result.join()}
+    }).done(function (data) {
+        var employees = JSON.parse(data);
+        if (data.length > 0) {
+            $('#selectId')
+                .find('option')
+                .remove()
+                .end();
+            $.each(employees, function (index, element) {
+                if (index == 0) {
+                    $("#selectId").append("<option value='" + element["post_id"] + "' disabled selected>" + element["post_name"] + "</option>");
+                }
+
+                $("#selectId").append("<option value='" + element["post_id"] + "'>" + element["post_name"] + "</option>");
+            });
+
+            $('#selectId').material_select();
+        }
     });
 }

0387a486e82eaf57029661de1b79c1834c956369
Egor Semenchenia <gorych911@gmail.com>
2015-11-22 13:45:39 +0300

    Added new attributes

--- src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
+++ src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
@@ -6,9 +6,13 @@ public final class AttributeConstants {
 
 
     public static final String INTERVIEW = "interview";
+    public static final String USER_INTERVIEW_HELPER = "user_interview_helper";
+    public static final String USER_INTERVIEW = "user_interview";
 
 
     public static final String EMPLOYEES = "employees";
+    public static final String INTERVIEWS = "interviews";
+    public static final String POSTS = "posts";
     public static final String SUBDIVISIONS = "subdivisions";
 
     private AttributeConstants() {

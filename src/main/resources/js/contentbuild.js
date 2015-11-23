ec60de9a3eca9989c69c5231da82b99c7739c988
Egor Semenchenia <gorych911@gmail.com>
2015-11-23 17:21:37 +0300

    Edited addAnswer method

--- src/main/resources/js/contentbuild.js
+++ src/main/resources/js/contentbuild.js
@@ -1,93 +1,93 @@
-$(document).ready(function () {
+function buildForm(interviewId) {
+    $.ajax({
+        url: '/create-question',
+        method: 'GET'
+    }).done(function (questionId) {
+        if (questionId > -1) {
+            var answersBoxId = "answers" + questionId;
+            var questionCssId = "#" + questionId;
 
-    $('select').material_select();
+            $('<form/>', {
+                id: questionId,
+                class: 'question',
+                method: 'get',
+                action: '/create-new-form'
+            }).appendTo('#interview_questions');
 
-    $('#addQuestionBtn').click(function () {
-        $.ajax({
-            url: '/create-question',
-            method: 'GET'
-        }).done(function (questionId) {
-            if (questionId > -1) {
-                var answersBoxId = "answers" + questionId;
-                var questionCssId = "#" + questionId;
+            $(questionCssId).append("<div class='row'><div class='input-field col s12'>" +
+                "<input id='questionText' type='text' length='200' name='questionName'>" +
+                "<label for='questionText'>Question</label></div></div>").
+                append("<input type='hidden' name='questionId' value='" + questionId + "'/>");
 
-                $('<form/>', {
-                    id: questionId,
-                    class: 'question'
-                }).appendTo('#interview_questions');
+            $('<div/>', {
+                id: answersBoxId
+            }).appendTo(questionCssId);
 
-                $(questionCssId).append("<div class='row'><div class='input-field col s12'>" +
-                    "<input id='questionText' type='text' length='200'>" +
-                    "<label for='questionText'>Question</label></div></div>");
+            var answerBtnId = "addAnswerBtn" + questionId;
 
-                $('<div/>', {
-                    id: answersBoxId
-                }).appendTo(questionCssId);
+            $('<a/>', {
+                id: answerBtnId,
+                class: 'btn-floating btn-middle waves-effect waves-light green'
+            }).appendTo(questionCssId);
 
-                var answerBtnId = "addAnswerBtn" + questionId;
+            $("#" + answerBtnId)
+                .attr("href", "JavaScript:addAnswer('" + answersBoxId + "','" + interviewId + "','" + questionId + "')")
+                .append("<i class='material-icons'>add</i>");
 
-                $('<a/>', {
-                    id: answerBtnId,
-                    class: 'btn-floating btn-middle waves-effect waves-light green'
-                }).appendTo(questionCssId).click(function () {
-                    $.ajax({
-                        url: '/create-answer',
-                        method: 'GET'
-                    }).done(function (answerId) {
-                        if (answerId > -1) {
-                            addAnswer(answerId, answersBoxId);
-                        }
-                    });
-                });
+            $('<div/>', {
+                class: 'divider divider-margin-fix'
+            }).appendTo(questionCssId);
 
-                $("#" + answerBtnId).append("<i class='material-icons'>add</i>");
-
-                $('<div/>', {
-                    class: 'divider divider-margin-fix'
-                }).appendTo(questionCssId);
-
-                $('<div/>', {
-                    class: 'right-align'
-                }).appendTo(questionCssId).append(
-                    "<a href='#' class='modal-action modal-close waves-effect waves-green btn-flat'>Save</a>" +
-                    "<a href='#' class='modal-action modal-close waves-effect waves-red btn-flat'>Delete</a>");
-            }
-        });
+            $('<div/>', {
+                class: 'right-align'
+            }).appendTo(questionCssId).append(
+                "<input type='submit' class='waves-effect waves-green btn-flat' value='Save'/>" +
+                "<a href='#' class='waves-effect waves-red btn-flat'>Delete</a>");
+        }
     });
-});
+}
 
-function addAnswer(answerId, parentId) {
+function addAnswer(parentId, interviewId, questionId) {
     var parentCssId = "#" + parentId;
-    var typeId = "type" + answerId;
-    var textId = "text" + answerId;
+    var url = "/create-answer/" + interviewId + "/" + questionId;
+    $.ajax({
+        url: url,
+        method: 'GET'
+    }).done(function (answerId) {
+        if (answerId > -1) {
+            var typeId = "type" + answerId;
+            var textId = "text" + answerId;
 
-    $('<div/>', {
-        id: answerId,
-        class: 'row'
-    }).appendTo(parentCssId);
+            $('<div/>', {
+                id: answerId,
+                class: 'row'
+            }).appendTo(parentCssId);
 
-    var elementCssId = "#" + answerId;
+            var elementCssId = "#" + answerId;
 
-    $('<div/>', {
-        class: 'input-field col l5 input-field-margin-fix'
-    }).appendTo(elementCssId).append("<select><option value='' disabled selected>Choose answer type</option>" +
-        "<option value='1'>Text field</option>" +
-        "<option value='2'>Radio Button</option>" +
-        "<option value='3'>Checkbox</option>" +
-        "<option value='4'>Slider</option></select>");
+            $('<div/>', {
+                class: 'input-field col l5 input-field-margin-fix'
+            }).appendTo(elementCssId).append("<select name='answerType' id='" + typeId + "'>" +
+                "<option value='' disabled selected>Choose answer type</option>" +
+                "<option value='1'>Text field</option>" +
+                "<option value='2'>Radio Button</option>" +
+                "<option value='3'>Checkbox</option>" +
+                "<option value='4'>Slider</option></select>");
 
-    $('<div/>', {
-        class: 'input-field col l6 input-field-margin-fix'
-    }).appendTo(elementCssId).append("<input id='" + textId + "' type='text'>" +
-        "<label for='" + textId + "'>Answer text</label>" +
-        "</div>");
+            $('<div/>', {
+                class: 'input-field col l6 input-field-margin-fix'
+            }).appendTo(elementCssId).append("<input id='" + textId + "' type='text' name='answerText'>" +
+                "<label for='" + textId + "'>Answer text</label></div>")
+                .append("<input type='hidden' name='answerId' value='" + answerId + "'/>");
 
-    $('<a/>', {
-        class: 'btn-floating btn waves-effect red delete-answer-lotion-btn-fix',
-        href: "#"
-    }).appendTo(elementCssId).append("<i class='material-icons'>delete</i>").click(function () {
-        $(elementCssId).remove();
+            $('<a/>', {
+                class: 'btn-floating btn waves-effect red delete-answer-lotion-btn-fix',
+                href: "#"
+            }).appendTo(elementCssId).append("<i class='material-icons'>delete</i>").click(function () {
+                $(elementCssId).remove();
+            });
+
+            $('select').material_select();
+        }
     });
-
-    $('select').material_select();
 }

e30d1be72475e8c4d7b528fef0bc409783799775
Egor Semenchenia <gorych911@gmail.com>
2015-11-25 22:40:20 +0300

    UserInterviewHelper renamed to ExtendUserInterview. Deleted Interview field.

---
+++ src/main/java/by/gstu/interviewstreet/domain/ExtendUserInterview.java
@@ -0,0 +1,23 @@
+package by.gstu.interviewstreet.domain;
+
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+public class ExtendUserInterview extends UserInterview {
+
+    @NotNull
+    Set<Post> posts;
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "posts=" + posts + '}';
+    }
+}

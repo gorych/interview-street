e4c991d8388587c4db20932c5c3d14ccec8cadd2
Egor Semenchenia <gorych911@gmail.com>
2015-11-25 22:41:03 +0300

    Added success response body attribute

--- src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
+++ src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
@@ -6,17 +6,18 @@ public final class AttributeConstants {
 
 
     public static final String INTERVIEW = "interview";
-    public static final String INTERVIEW_ID = "interview_id";
-    public static final String USER_INTERVIEW_HELPER = "user_interview_helper";
-    public static final String USER_INTERVIEW = "user_interview";
+    public static final String USER_INTERVIEW = "extendUserInterview";
 
 
     public static final String POSTS = "posts";
     public static final String QUESTION_FORMS = "forms";
-    public static final String ANSWER_FORMS = "answer_forms";
-    public static final String EMPLOYEES = "employees";
     public static final String INTERVIEWS = "interviews";
+    public static final String ANSWER_FORMS = "answer_forms";
     public static final String SUBDIVISIONS = "subdivisions";
+
+
+    public static final String SUCCESS_RESPONSE_BODY= "success";
+
 
     private AttributeConstants() {
     }

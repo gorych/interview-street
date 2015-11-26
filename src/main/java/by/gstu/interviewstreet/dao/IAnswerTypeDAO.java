a00cf4783737b0ed894153e33912e03eb8d4dd30
Egor Semenchenia <gorych911@gmail.com>
2015-11-27 00:38:41 +0300

    Deleted getAnswerTypesByIds method

--- src/main/java/by/gstu/interviewstreet/dao/IAnswerTypeDAO.java
+++ src/main/java/by/gstu/interviewstreet/dao/IAnswerTypeDAO.java
@@ -2,14 +2,10 @@ package by.gstu.interviewstreet.dao;
 
 import by.gstu.interviewstreet.domain.AnswerType;
 
-import java.util.List;
-
 public interface IAnswerTypeDAO {
 
     AnswerType getDefaultAnswerType();
 
     AnswerType getAnswerTypeById(int id);
-
-    List<AnswerType> getAnswerTypesByIds(Integer[] ids);
 
 }
--- src/main/java/by/gstu/interviewstreet/dao/impl/AnswerTypeDAOImpl.java
+++ src/main/java/by/gstu/interviewstreet/dao/impl/AnswerTypeDAOImpl.java
@@ -8,9 +8,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
 public class AnswerTypeDAOImpl implements IAnswerTypeDAO {
 
@@ -30,18 +27,5 @@ public class AnswerTypeDAOImpl implements IAnswerTypeDAO {
         query.setInteger("id", id);
 
         return (AnswerType) query.uniqueResult();
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<AnswerType> getAnswerTypesByIds(Integer[] ids) {
-        List<AnswerType> answerTypes = new ArrayList<>();
-        Query query = sessionFactory.getCurrentSession().createQuery("FROM AnswerType WHERE id = :id");
-        for (Integer id : ids) {
-            query.setInteger("id", id);
-            answerTypes.add((AnswerType) query.uniqueResult());
-        }
-
-        return answerTypes;
     }
 }

a8e9abf3fef4c78bdd2ff8091aced9b6a9507d29
Egor Semenchenia <gorych911@gmail.com>
2015-11-27 16:46:25 +0300

    Created getJsonString method

--- src/main/java/by/gstu/interviewstreet/service/impl/FormServiceImpl.java
+++ src/main/java/by/gstu/interviewstreet/service/impl/FormServiceImpl.java
@@ -3,15 +3,18 @@ package by.gstu.interviewstreet.service.impl;
 import by.gstu.interviewstreet.dao.IAnswerDAO;
 import by.gstu.interviewstreet.dao.IFormDAO;
 import by.gstu.interviewstreet.dao.IQuestionDAO;
-import by.gstu.interviewstreet.domain.Answer;
-import by.gstu.interviewstreet.domain.Form;
-import by.gstu.interviewstreet.domain.Question;
+import by.gstu.interviewstreet.domain.*;
 import by.gstu.interviewstreet.service.FormService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class FormServiceImpl implements FormService {
@@ -37,5 +40,45 @@ public class FormServiceImpl implements FormService {
         List<Form> forms = formDAO.getByQuestion(question);
         answerDAO.remove(forms);
         questionDAO.remove(question);
+    }
+
+    @Override
+    @Transactional
+    public String getJsonString(int questionId) {
+        Question question = questionDAO.qetQuestionById(questionId);
+        List<Form> forms = formDAO.getByQuestion(question);
+        if (forms == null) {
+            return "";
+        }
+
+        StringBuilder answerTexts = new StringBuilder();
+        StringBuilder answerIds = new StringBuilder();
+        for (Form form : forms) {
+            String text = form.getAnswer().getText();
+            int id = form.getAnswer().getId();
+
+            answerTexts.append(text).append(",");
+            answerIds.append(id).append(",");
+        }
+        answerTexts.deleteCharAt(answerTexts.length() - 1);
+        answerIds.deleteCharAt(answerIds.length() - 1);
+
+        List<Map<String, Object>> jsonList = new ArrayList<>();
+        Map<String, Object> jsonObject = new HashMap<>();
+
+        Form form = forms.get(0);
+        jsonObject.put("type", form.getAnswer().getType().getId());
+        jsonObject.put("question", form.getQuestion().getText());
+        jsonObject.put("answers", answerTexts);
+        jsonObject.put("answer_ids", answerIds);
+
+        jsonList.add(jsonObject);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(jsonList);
+        } catch (JsonProcessingException e) {
+            return "";
+        }
     }
 }

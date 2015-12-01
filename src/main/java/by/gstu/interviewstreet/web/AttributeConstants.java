b835dd468c2dfa8ba839c7e47eb92086326bc7dd
Egor Semenchenia <gorych911@gmail.com>
2015-12-01 15:43:13 +0300

    Added constants for user interview and error body

--- src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
+++ src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
@@ -14,12 +14,14 @@ public final class AttributeConstants {
     public static final String POSTS = "posts";
     public static final String QUESTION_FORMS = "forms";
     public static final String INTERVIEWS = "interviews";
+    public static final String USER_INTERVIEWS = "userInterviews";
     public static final String ANSWER_FORMS = "answer_forms";
     public static final String SUBDIVISIONS = "subdivisions";
 
 
-    public static final String SUCCESS_RESPONSE_BODY= "success";
-    public static final String EMPTY_BODY= "";
+    public static final String SUCCESS_RESPONSE_BODY = "success";
+    public static final String EMPTY_BODY = "";
+    public static final int ERROR_BODY = -1;
 
 
     private AttributeConstants() {

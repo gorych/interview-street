39761ec8ec82057737ead20f1ea67e7271d6d494
Egor Semenchenia <gorych911@gmail.com>
2015-12-02 23:25:53 +0300

    Added ParameterConstants class

---
+++ src/main/java/by/gstu/interviewstreet/web/ParameterConstants.java
@@ -0,0 +1,11 @@
+package by.gstu.interviewstreet.web;
+
+public class ParameterConstants {
+
+    public static final String QUESTION_TEXT = "questionText";
+    public static final String QUESTION_ID = "questionId";
+    public static final String ANSWER_TYPE_ID = "typeId";
+    public static final String ANSWER_TEXT = "answerText";
+    public static final String ANSWER_ID = "answerId";
+
+}

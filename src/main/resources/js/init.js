8ca467eee8df1b972f2f56cf57d0acc447b64cf3
Yahor <gorych911@gmail.com>
2016-03-22 02:10:22 +0300

    Edit init.js

--- src/main/resources/js/init.js
+++ src/main/resources/js/init.js
@@ -6,14 +6,26 @@
         });
         $('.button-collapse').sideNav();
 
-        /*Default min date for datepicker*/
+        /*Min date for datepicker*/
         var tomorrow = new Date();
         tomorrow.setDate(tomorrow.getDate() + 1);
 
         $('.datepicker').pickadate({
+            // Strings and translations
+            monthsFull: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+            weekdaysFull: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+            weekdaysShort: ['Пон', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вскр'],
+            showMonthsShort: undefined,
+            showWeekdaysFull: undefined,
+
+            // Buttons
+            today: null,
+            clear: 'Сброс',
+            close: 'Выбрать',
             selectYears: 10,
-            format: 'dd-mmm-yyyy',
-            formatSubmit: 'yyyy/mm/dd',
+            format: 'yyyy-mm-dd',
+            formatSubmit: 'yyyy-mm-dd',
             min: tomorrow
         });
         $('select').material_select();

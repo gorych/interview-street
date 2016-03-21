f9445434ebc18932c47d1889b640e9c40748dd52
Yahor <gorych911@gmail.com>
2016-03-22 02:15:18 +0300

    Del unused method

--- src/main/java/by/gstu/interviewstreet/web/controller/GlobalControllerAdvice.java
+++ src/main/java/by/gstu/interviewstreet/web/controller/GlobalControllerAdvice.java
@@ -1,10 +1,10 @@
 package by.gstu.interviewstreet.web.controller;
 
 import by.gstu.interviewstreet.domain.User;
-import by.gstu.interviewstreet.domain.ExtendUserInterview;
 import by.gstu.interviewstreet.service.UserService;
 import by.gstu.interviewstreet.web.AttributeConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,12 +20,8 @@ public class GlobalControllerAdvice {
     public String addUserInitials() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userService.get(username);
-        return user != null ? user.getEmployee().getInitials() : "ANONYMOUS";
-    }
 
-    @ModelAttribute
-    public void addFormData(Model model) {
-        model.addAttribute(AttributeConstants.USER_INTERVIEW, new ExtendUserInterview());
+        return user != null ? user.getEmployee().getInitials() : "Аноним";
     }
 
 }

b7503fce85a4078e6dea71f8dcdc742e45d99260
Yahor <gorych911@gmail.com>
2016-04-04 01:07:22 +0300

    Add class for service utils

---
+++ src/main/java/by/gstu/interviewstreet/service/ServiceUtils.java
@@ -0,0 +1,24 @@
+package by.gstu.interviewstreet.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class ServiceUtils {
+
+    private ServiceUtils() {
+    }
+
+    public static Map<String, Object> buildValueMap(String[] keys, Object[] objects) throws IllegalArgumentException {
+        if (keys.length != objects.length) {
+            throw new IllegalArgumentException("Array length are not equal");
+        }
+
+        Map<String, Object> valueMap = new HashMap<>();
+        for (int i = 0; i < keys.length ; i++) {
+            valueMap.put(keys[i], objects[i]);
+        }
+
+        return valueMap;
+    }
+
+}

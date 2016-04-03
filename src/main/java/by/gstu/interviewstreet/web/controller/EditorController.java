7ca3b06889b642044064f34dedb2e296fc27acb8
Yahor <gorych911@gmail.com>
2016-04-03 11:53:33 +0300

    Add userInitials and showDesigner methods

--- src/main/java/by/gstu/interviewstreet/web/controller/EditorController.java
+++ src/main/java/by/gstu/interviewstreet/web/controller/EditorController.java
@@ -2,9 +2,8 @@ package by.gstu.interviewstreet.web.controller;
 
 
 import by.gstu.interviewstreet.dao.IInterviewTypeDAO;
-import by.gstu.interviewstreet.domain.AnswerType;
-import by.gstu.interviewstreet.domain.Employee;
-import by.gstu.interviewstreet.domain.Interview;
+import by.gstu.interviewstreet.domain.*;
+import by.gstu.interviewstreet.security.UserRoleConstants;
 import by.gstu.interviewstreet.service.*;
 import by.gstu.interviewstreet.web.AttributeConstants;
 import by.gstu.interviewstreet.web.util.JSONParser;
@@ -12,17 +11,24 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Type;
+import java.security.Principal;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Controller
-//@Secured(UserRoleConstants.EDITOR)
+@Secured(UserRoleConstants.EDITOR)
 public class EditorController {
+
+    @Autowired
+    UserService userService;
 
     @Autowired
     AnswerService answerService;
@@ -45,6 +51,14 @@ public class EditorController {
     @Autowired
     UserInterviewService userInterviewService;
 
+    @ModelAttribute(AttributeConstants.USER_INITIALS)
+    public String addUserInitials(Principal principal) {
+        String username = principal.getName();
+        User user = userService.get(username);
+
+        return user.getEmployee().getInitials();
+    }
+
     @RequestMapping(value = {"/interview-list"}, method = RequestMethod.GET)
     public String showInterviewList(Model model) {
         model.addAttribute(AttributeConstants.INTERVIEWS, interviewService.getAll());
@@ -58,7 +72,8 @@ public class EditorController {
     public String loadPosts(@RequestBody String data) {
         JsonArray jsonArray = JSONParser.convertJsonStringToJsonArray(data);
 
-        Type type = new TypeToken<List<Integer>>() { }.getType();
+        Type type = new TypeToken<List<Integer>>() {
+        }.getType();
         List<Integer> subdivisionIds = JSONParser.convertJsonElementToObject(jsonArray, type);
         List<Employee> employees = employeeService.getBySubdivisions(subdivisionIds);
 
@@ -109,31 +124,36 @@ public class EditorController {
         return JSONParser.convertObjectToJsonString(valueMap);
     }
 
+    @RequestMapping(value = {"/{hash}/designer"}, method = RequestMethod.GET)
+    public String showDesigner(@PathVariable String hash, Model model) {
+        Interview interview = interviewService.get(hash);
+        model.addAttribute(AttributeConstants.INTERVIEW, interview);
+
+        return "designer";
+    }
+
     @ResponseBody
-    @RequestMapping(value = {"/build-form"}, method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
-    public String buildQuestionForm(int hash, int answerTypeId) {
-        AnswerType answerType;
-        Interview interview;
+    @RequestMapping(value = {"/designer/add-question"}, method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
+    public String addQuestion(String hash, int answerTypeId, int number) {
         try {
-            answerType = answerService.getAnswerType(answerTypeId);
-            interview = interviewService.get(hash);
+            AnswerType answerType = answerService.getAnswerType(answerTypeId);
+            Interview interview = interviewService.get(hash);
+
+            if (interview == null || answerType == null) {
+                return AttributeConstants.ERROR_RESPONSE_BODY;
+            }
+
+            Question question = questionService.add(interview, number);
+            answerService.addDefaultAnswers(answerType, question);
+
+            /*Get question with answers*/
+            Question fullQuestion = questionService.get(question.getId());
+
+            return JSONParser.convertObjectToJsonString(fullQuestion);
+
         } catch (RuntimeException e) {
             return AttributeConstants.ERROR_RESPONSE_BODY;
         }
-
-        if (interview == null || answerType == null) {
-            return AttributeConstants.ERROR_RESPONSE_BODY;
-        }
-
-        Map<String, Object> valueMap = null;//formService.getValueMapForForm(interview, answerType);
-
-        return JSONParser.convertObjectToJsonString(valueMap);
-    }
-
-    @RequestMapping(value = {"/{interviewId}/designer"}, method = RequestMethod.GET)
-    public String showQuestionsEditor(@PathVariable int interviewId, Model model) {
-
-        return "test";
     }
 
     //endregion

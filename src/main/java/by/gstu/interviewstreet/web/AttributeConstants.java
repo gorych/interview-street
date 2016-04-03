17785396308977234fea8ab1eabb518113733aab
Yahor <gorych911@gmail.com>
2016-04-03 23:26:09 +0300

    Del unused attributes

--- src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
+++ src/main/java/by/gstu/interviewstreet/web/AttributeConstants.java
@@ -6,16 +6,11 @@ public final class AttributeConstants {
 
 
     public static final String INTERVIEW = "interview";
-    public static final String SUBDIVISION = "subdivision";
-    public static final String TYPE = "type";
     public static final String CHIP = "chip";
 
 
-    public static final String POSTS = "posts";
-    public static final String QUESTION_FORMS = "forms";
+    public static final String QUESTIONS = "questions";
     public static final String INTERVIEWS = "interviews";
-    public static final String USER_INTERVIEWS = "userInterviews";
-    public static final String ANSWER_FORMS = "answer_forms";
     public static final String SUBDIVISIONS = "subdivisions";
 
 

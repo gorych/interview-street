bee4154d54288ab46d6f24ce3a14e57b1c6ec668
Yahor <gorych911@gmail.com>
2016-04-07 20:24:37 +0300

    Fix

---
+++ src/main/java/by/gstu/interviewstreet/dao/impl/AnswerTypDAOImpl.java
@@ -0,0 +1,20 @@
+package by.gstu.interviewstreet.dao.impl;
+
+import by.gstu.interviewstreet.dao.AnswerTypeDAO;
+import by.gstu.interviewstreet.domain.AnswerType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AnswerTypDAOImpl extends AbstractDbDAO implements AnswerTypeDAO {
+
+
+
+    @Override
+    public AnswerType getById(int id) {
+        return (AnswerType) getSession()
+                .createQuery("FROM AnswerType WHERE id = :id")
+                .setInteger("id", id)
+                .uniqueResult();
+    }
+
+}

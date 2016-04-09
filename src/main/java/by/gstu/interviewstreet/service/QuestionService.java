cfead27af2324dea922105dbb7fe94bfaf6a103d
Yahor <gorych911@gmail.com>
2016-04-09 12:38:02 +0300

    Add gt method

--- src/main/java/by/gstu/interviewstreet/service/QuestionService.java
+++ src/main/java/by/gstu/interviewstreet/service/QuestionService.java
@@ -8,10 +8,13 @@ import by.gstu.interviewstreet.domain.QuestionType;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public interface QuestionService {
 
     Question get(int id);
+
+    Question get(String hash, int questId);
 
     QuestionType getType(int typeId);
 
--- src/main/java/by/gstu/interviewstreet/service/impl/QuestionServiceImpl.java
+++ src/main/java/by/gstu/interviewstreet/service/impl/QuestionServiceImpl.java
@@ -1,28 +1,50 @@
 package by.gstu.interviewstreet.service.impl;
 
+import by.gstu.interviewstreet.dao.InterviewDAO;
 import by.gstu.interviewstreet.dao.QuestionDAO;
 import by.gstu.interviewstreet.dao.QuestionTypeDAO;
-import by.gstu.interviewstreet.domain.*;
+import by.gstu.interviewstreet.domain.Interview;
+import by.gstu.interviewstreet.domain.Question;
+import by.gstu.interviewstreet.domain.QuestionType;
 import by.gstu.interviewstreet.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
     @Autowired
-    private QuestionTypeDAO questionTypeDAO;
+    private QuestionDAO questionDAO;
 
     @Autowired
-    private QuestionDAO questionDAO;
+    private InterviewDAO interviewDAO;
+
+    @Autowired
+    private QuestionTypeDAO questionTypeDAO;
 
     @Override
     @Transactional
     public Question get(int id) {
         return questionDAO.getById(id);
+    }
+
+    @Override
+    @Transactional
+    public Question get(String hash, int questId) {
+        Interview interview = interviewDAO.getByHash(hash);
+        Question question = questionDAO.getById(questId);
+
+        Set<Question> questions = interview.getQuestions();
+        if (questions.contains(question)) {
+            return question;
+        }
+
+        throw new IllegalArgumentException("User is trying to get a nonexistent question by " +
+                "id = " + questId + ". Interview hash = " + hash);
     }
 
     @Override

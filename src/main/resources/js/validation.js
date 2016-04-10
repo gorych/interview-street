0a7f07d595a4cb07800591fa4b111daffb3f41b6
Yahor <gorych911@gmail.com>
2016-04-11 02:07:53 +0300

    Validation migrated to separate file validation.js

---
+++ src/main/resources/js/validation.js
@@ -0,0 +1,56 @@
+var validator = validator || {};
+
+(function () {
+
+    validator.toggleSelectValidateClass = function (selector) {
+        var hiddenInput = $(selector).prevAll("input.select-dropdown");
+        var value = $(selector).val();
+
+        if (!value || value.length < 1) {
+            $(hiddenInput)
+                .removeClass("valid valid-select")
+                .addClass("invalid invalid-select");
+        } else {
+            $(hiddenInput)
+                .removeClass("invalid invalid-select")
+                .addClass("valid valid-select");
+        }
+    };
+
+    validator.toggleInputValidateClass = function (input) {
+        if (!input.value || input.validity.patternMismatch) {
+            $(input).removeClass("valid").addClass("invalid");
+        } else {
+            $(input).removeClass("invalid").addClass("valid");
+        }
+    };
+
+    /*Check date with type 'date'*/
+    validator.isValidDate = function (dateField) {
+        var dateInput = dateField[0];
+        var hiddenInput = document.getElementsByName(dateInput.name + "_submit")[0];
+
+        if (!hiddenInput.value || hiddenInput.validity.patternMismatch) {
+            dateInput.classList.remove("valid");
+            dateInput.classList.add("invalid");
+        } else {
+            dateInput.classList.remove("invalid");
+            dateInput.classList.add("valid");
+        }
+    };
+
+    validator.isValidFields = function () {
+        var $form = $("#add-interview-form");
+
+        $form.find("[required]").each(function (i, elem) {
+            if ($(elem).is("select")) {
+                validator.toggleSelectValidateClass(elem);
+            } else {
+                validator.toggleInputValidateClass(elem);
+            }
+        });
+
+        return ($form.find("input.invalid").length < 1);
+    };
+
+}());

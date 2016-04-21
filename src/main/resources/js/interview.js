836bacf1c6e8a05d2f009d95938c9f1a72ed88c0
Yahor <gorych911@gmail.com>
2016-04-22 00:35:45 +0300

    Add answer which collect question data

--- src/main/resources/js/interview.js
+++ src/main/resources/js/interview.js
@@ -37,10 +37,38 @@
         $(this).closest(".col").prev().find("input").prop("checked", true);
     });
 
-    $("#send-form-btn").click(function(){
-        if(!validator.isCorrectForm()){
+    $("#send-form-btn").click(function () {
+        if (!validator.isCorrectForm()) {
             return;
         }
+
+        var questions = [];
+        /*Collect data to server*/
+        $("[data-quest]").each(function () {
+            var questId = $(this).attr("data-quest");
+
+            var answers = [];
+            $(this).find("input:checked").each(function () {
+                var label = $(this).next();
+
+                var id = $(this).attr("id");
+                var text = ($(label).is("label") && !$(label).hasClass("optional-label"))
+                    ? $(label).html() : $(this).parent().next().find("input").val();
+                answers.push({id: id, text: text});
+            });
+
+            $(this).find("input[type='hidden']").each(function () {
+                answers.push({id: $(this).attr("id"), text: $(this).val()})
+            });
+
+            $(this).find("input[type='text']:not('.optional-text')").each(function () {
+                answers.push({id: $(this).attr("id"), text: $(this).val()})
+            });
+
+            questions.push({id: questId, answers: answers});
+        });
+
+        alert(JSON.stringify(questions));
     });
 
 

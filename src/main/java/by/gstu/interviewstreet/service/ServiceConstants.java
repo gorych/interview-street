3230b46931252d5e90ef56246ee8efc3c6356943
Yahor <gorych911@gmail.com>
2016-04-22 18:47:06 +0300

    Add class for service constants

---
+++ src/main/java/by/gstu/interviewstreet/service/ServiceConstants.java
@@ -0,0 +1,10 @@
+package by.gstu.interviewstreet.service;
+
+public final class ServiceConstants {
+
+    private ServiceConstants() {
+    }
+
+    public static final String TEXT_ANSWER_NAME = "text";
+
+}

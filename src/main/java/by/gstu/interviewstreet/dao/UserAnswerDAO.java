023753ed6fe4bbabad7a57667683db2b2f998b74
Yahor <gorych911@gmail.com>
2016-04-25 01:02:22 +0300

    Add getAnswersByQuestion method

--- src/main/java/by/gstu/interviewstreet/dao/UserAnswerDAO.java
+++ src/main/java/by/gstu/interviewstreet/dao/UserAnswerDAO.java
@@ -1,9 +1,14 @@
 package by.gstu.interviewstreet.dao;
 
+import by.gstu.interviewstreet.domain.Question;
 import by.gstu.interviewstreet.domain.UserAnswer;
+
+import java.util.List;
 
 public interface UserAnswerDAO {
 
     void saveOrUpdate(UserAnswer userAnswer);
+
+    List<UserAnswer> getAnswersByQuestion(Question question);
 
 }
--- src/main/java/by/gstu/interviewstreet/dao/impl/UserAnswerDAOImpl.java
+++ src/main/java/by/gstu/interviewstreet/dao/impl/UserAnswerDAOImpl.java
@@ -1,8 +1,12 @@
 package by.gstu.interviewstreet.dao.impl;
 
 import by.gstu.interviewstreet.dao.UserAnswerDAO;
+import by.gstu.interviewstreet.domain.Question;
 import by.gstu.interviewstreet.domain.UserAnswer;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class UserAnswerDAOImpl extends AbstractDbDAO implements UserAnswerDAO {
@@ -10,5 +14,15 @@ public class UserAnswerDAOImpl extends AbstractDbDAO implements UserAnswerDAO {
     @Override
     public void saveOrUpdate(UserAnswer userAnswer) {
         getSession().saveOrUpdate(userAnswer);
+    }
+
+    @Override
+    @Transactional
+    @SuppressWarnings("unchecked")
+    public List<UserAnswer> getAnswersByQuestion(Question question) {
+        return getSession()
+                .createQuery("FROM UserAnswer WHERE question =:question GROUP BY answerText")
+                .setEntity("question", question)
+                .list();
     }
 }

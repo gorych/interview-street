e32d481cae4624b5c9fa40f3e792bfa39f856fbb
Yahor_Semianchenia <yahor_semianchenia@epam.com>
2016-04-29 16:23:58 +0300

    Add fetByInterviewHash method

--- src/main/java/by/gstu/interviewstreet/service/UserInterviewService.java
+++ src/main/java/by/gstu/interviewstreet/service/UserInterviewService.java
@@ -3,10 +3,20 @@ package by.gstu.interviewstreet.service;
 import by.gstu.interviewstreet.domain.Interview;
 import by.gstu.interviewstreet.domain.UserInterview;
 
+import java.util.List;
+
 public interface UserInterviewService {
 
     void addInterviewToUserByPost(Interview interview, Integer[] postIds, Integer[] subIds);
 
     UserInterview getByUserAndInterview(String username, String hash);
+
+    List<UserInterview> getByInterviewHash(String hash);
+
+    List<UserInterview> sortByEmployeeLastname(List<UserInterview> userInterviews, SortType sortType);
+
+    List<UserInterview> sortByDate(List<UserInterview> userInterviews, SortType sortType);
+
+    List<UserInterview> sortByStatus(List<UserInterview> userInterviews, SortType sortType);
 
 }

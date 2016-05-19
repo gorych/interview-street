621fd05e58fc5826189e37da6be75c9f4b9d6229
Yahor_Semianchenia <yahor_semianchenia@epam.com>
2016-05-19 16:47:15 +0300

    Small refactoring

--- src/main/java/by/gstu/interviewstreet/domain/UserInterview.java
+++ src/main/java/by/gstu/interviewstreet/domain/UserInterview.java
@@ -9,7 +9,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
 
 @Entity
 @Table(name = "user_interviews")

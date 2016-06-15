04abfa556c85d27155a7567b1bc3106c8a32ed89
Yahor <gorych911@gmail.com>
2016-06-15 23:30:05 +0300

    Edit logic for write excel book

--- src/main/java/by/gstu/interviewstreet/service/ExportToExcelService.java
+++ src/main/java/by/gstu/interviewstreet/service/ExportToExcelService.java
@@ -1,13 +1,16 @@
 package by.gstu.interviewstreet.service;
 
+import by.gstu.interviewstreet.bean.StatisticData;
 import by.gstu.interviewstreet.domain.Interview;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public interface ExportToExcelService {
 
-    XSSFWorkbook exportAllStatistics(Interview interview) throws IOException;
+    XSSFWorkbook exportAllStatistics(List<StatisticData> statistics, Interview interview, ByteArrayOutputStream out) throws IOException;
 
 }
 
--- src/main/java/by/gstu/interviewstreet/service/impl/ExportToExcelServiceImpl.java
+++ src/main/java/by/gstu/interviewstreet/service/impl/ExportToExcelServiceImpl.java
@@ -4,6 +4,7 @@ import by.gstu.interviewstreet.bean.StatisticData;
 import by.gstu.interviewstreet.domain.Interview;
 import by.gstu.interviewstreet.service.ExportToExcelService;
 import by.gstu.interviewstreet.service.StatisticsService;
+import by.gstu.interviewstreet.service.UserAnswerService;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -32,10 +34,12 @@ public class ExportToExcelServiceImpl implements ExportToExcelService {
     @Autowired
     StatisticsService statisticsService;
 
+    @Autowired
+    UserAnswerService userAnswerService;
+
     @Override
-    public XSSFWorkbook exportAllStatistics(Interview interview) throws IOException {
+    public XSSFWorkbook exportAllStatistics(List<StatisticData> statistics, Interview interview, ByteArrayOutputStream out) throws IOException {
          /*NULL is correct value*/
-        List<StatisticData> statistics = statisticsService.getInterviewStatistics(interview, null, null);
         try (XSSFWorkbook book = new XSSFWorkbook()) {
             XSSFSheet sheet = book.createSheet(SHEET_NAME);
             int rowNumber = 0;
@@ -43,7 +47,7 @@ public class ExportToExcelServiceImpl implements ExportToExcelService {
             XSSFRow mainHeader = sheet.createRow(rowNumber++);
             XSSFCell cell = mainHeader.createCell(0);
             cell.setCellType(Cell.CELL_TYPE_STRING);
-            cell.setCellValue(HEADER_PREFIX + "\"" + interview.getName() + "\"");
+            cell.setCellValue(HEADER_PREFIX + interview.getName() + "\"");
             makeCellAutosizeAndBold(book, mainHeader);
 
             rowNumber++;
@@ -59,14 +63,17 @@ public class ExportToExcelServiceImpl implements ExportToExcelService {
                 XSSFCell c1 = tableHeader.createCell(0);
                 XSSFCell c2 = tableHeader.createCell(1);
                 XSSFCell c3 = tableHeader.createCell(2);
+                XSSFCell c4 = tableHeader.createCell(3);
 
                 c1.setCellType(Cell.CELL_TYPE_STRING);
                 c2.setCellType(Cell.CELL_TYPE_NUMERIC);
                 c3.setCellType(Cell.CELL_TYPE_NUMERIC);
+                c4.setCellType(Cell.CELL_TYPE_STRING);
 
                 c1.setCellValue(ANSWER_CELL_HEADER);
                 c2.setCellValue(PEOPLE_COUNT_HEADER);
                 c3.setCellValue(PERCENT_HEADER);
+                c4.setCellValue("Ответившие респонденты");
 
                 makeCellAutosizeAndBold(book, tableHeader);
 
@@ -77,20 +84,27 @@ public class ExportToExcelServiceImpl implements ExportToExcelService {
                     XSSFCell answerText = answer.createCell(0);
                     XSSFCell peopleResponded = answer.createCell(1);
                     XSSFCell percentResponded = answer.createCell(2);
+                    XSSFCell respondents = answer.createCell(3);
 
                     answerText.setCellType(Cell.CELL_TYPE_STRING);
                     peopleResponded.setCellType(Cell.CELL_TYPE_NUMERIC);
                     percentResponded.setCellType(Cell.CELL_TYPE_NUMERIC);
+                    respondents.setCellType(Cell.CELL_TYPE_STRING);
 
                     answerText.setCellValue(key);
                     Object[] values = answerData.get(key);
 
                     peopleResponded.setCellValue(values[0].toString());
                     percentResponded.setCellValue(values[1].toString().replace(",", "."));
+                    respondents.setCellValue(userAnswerService.getRespondentListHowLine(interview, key));
+
+                    makeCellsAutosize(book, answer);
                 }
 
                 rowNumber++;
             }
+
+            book.write(out);
             return book;
         }
     }
@@ -103,6 +117,12 @@ public class ExportToExcelServiceImpl implements ExportToExcelService {
 
         for (int i = 0; i < row.getLastCellNum(); i++) {
             row.getCell(i).setCellStyle(style);
+        }
+        makeCellsAutosize(wb, row);
+    }
+
+    private void makeCellsAutosize(Workbook wb, Row row) {
+        for (int i = 0; i < row.getLastCellNum(); i++) {
             wb.getSheetAt(0).autoSizeColumn(i);
         }
     }
